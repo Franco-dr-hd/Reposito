f4ed3ed2b639b4f7d0dfe3e347351a041ba3f72c
Franco-dr-hd <derenzisjeanfranco@gmail.com>
2024-09-04 10:43:44 -0300

    creamos las directivas @Input y @Output; conectamos la información entre el componente padre e hijo; mostramos la información en un carrusel y una notificación

--- src/app/modules/producto/pages/producto/producto.component.ts
+++ src/app/modules/producto/pages/producto/producto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Producto } from 'src/app/models/producto';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-producto',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+  //String que modificara el valor de @Input en el componente hijo
+  product: string = '';
 
+  //Coleccion de productos añadidos a la lista
+  productoCarrusel: Producto[] = [];
+
+  productoAnadido(producto:Producto){
+
+    //Modificador del valor de ´product´
+    this.product = `${producto.nombre} : $${producto.precio}`
+
+    try {
+      /*agregamos la informacion por el parametro de la funcion a la coleccion
+      de carrusel */
+      this.productoCarrusel.push(producto);
+
+      Swal.fire({
+        title: 'Bien',
+        text: 'Ha añadido este producto con exito',
+        icon: 'info'
+      })
+    } catch (error) {
+      Swal.fire({
+        title: '¡Oh no!',
+        text: 'Ha ocurrido un error\n'+error,
+        icon: 'error'
+      })
+      
+    }
+  }
 }
