294086cc0fea08316ba441a792074a07b9f3d459
Franco-dr-hd <derenzisjeanfranco@gmail.com>
2024-09-12 11:12:51 -0300

    detalles cambiados en css para tamaño de imagenes

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// CONFIGURACIÓN DE FIREBASE CON NUESTRO PROYECTO -> VINCULAR
+export const environment = {
+    production: false,
+
+    // sacamos palabra "const" y ";" final y cambiamos el "=" por ":"
+    firebaseConfig: {
+        apiKey: "AIzaSyCd3MTUWZDl6VNEa_S-qaJKUQXcPTcFZhk",
+        authDomain: "project-dr-cb460.firebaseapp.com",
+        projectId: "project-dr-cb460",
+        storageBucket: "project-dr-cb460.appspot.com",
+        messagingSenderId: "724002615968",
+        appId: "1:724002615968:web:2abf4b6ef72045b6780fc5"
+      }
+}
